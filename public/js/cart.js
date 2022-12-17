d370b7c721836f8ec04b3b6e06664873ea07490a
HuyenTran <sonerin1090@gmail.com>
2022-12-17 13:41:13 +0700

    no message

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.showCart').on('click', function (e) {
-        showProductGallery()
+        showProductGallery();
     });
 });
 function showProductGallery() {
@@ -20,14 +20,14 @@ function showProductGallery() {
                         '<td data-th="Product">'+
                             '<div class=\"row\">'+
                                 '<div class=\"col-md-3 text-left\">'+
-                                    '<img src="http://127.0.0.1:8080/storage/images/'+cart.image+'" class=\"img-fluid d-none d-md-block rounded mb-2 shadow\">'+
+                                    '<img src="http://127.0.0.1:8080/storage/images/'+cart[i].image+'" class=\"img-fluid d-none d-md-block rounded mb-2 shadow\">'+
                                 '</div>'+
                                 '<div class=\"col-md-9 text-left mt-sm-2\">'+
-                                    '<h4>'+cart.name+'</h4>'+
+                                    '<h4>'+cart[i].name+'</h4>'+
                                 '</div>'+
                             '</div>'+
                         '</td>'+
-                        '<td data-th="Price">'+cart.price+'</td>'+
+                        '<td data-th="Price">'+cart[i].price+'</td>'+
                         '<td data-th="Quantity">'+
                             '<input type="number" class=\"form-control form-control-lg text-center\" value="1">'+
                         '</td>'+
