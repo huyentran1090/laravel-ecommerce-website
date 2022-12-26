07498e95b5137ec6fa2c56bf6997f78ce85018fe
HuyenTran <sonerin1090@gmail.com>
2022-12-26 20:53:46 +0700

    no message

---
+++ database/migrations/2022_12_19_203028_create_profile_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('image');
+            $table->date('date_of_birth');
+            $table->tinyInteger('gender');
+            $table->string('phone_number', 20)->nullable();
+            $table->string('address');
+            $table->string('social_network');
+            $table->unsignedInteger('id_user');
+            $table->foreign('id_user')
+            ->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
---
+++ database/migrations/2022_12_19_205208_add_paid_to_profile_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaidToProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('image');
+            $table->date('date_of_birth');
+            $table->tinyInteger('gender');
+            $table->string('phone_number', 20)->nullable();
+            $table->string('address');
+            $table->string('social_network');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')
+            ->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profile', function (Blueprint $table) {
+            //
+        });
+    }
+}
